9f4a454c1f7c6bf052413a3e6e140bfab9df7370
Keerthana Byreddy <byreddyk@msu.edu>
2024-12-08 19:07:20 -0500

    Removed the empty model and properly added the files to the github.

--- web app/app.py
+++ web app/app.py
@@ -6,91 +6,32 @@ import pandas as pd
 
 app = flask.Flask(__name__, template_folder='templates')
 
-model = tf.keras.models.load_model('model/my_model.keras')
 
-    
 @app.route('/', methods=['GET', 'POST'])
-
 def main():
-
     if flask.request.method == 'GET':
-
         return(flask.render_template('main.html'))
-
+    
     if flask.request.method == 'POST':
+        zone = flask.request.form['zone']
 
-        PULocationID= flask.request.form['PULocationID']
-        hour= flask.request.form['hour']
-        dayofweek= flask.request.form['dayofweek']
-        dayofyear= flask.request.form['dayofyear']
-        month= flask.request.form['month']
-        quarter= flask.request.form['quarter']
-        x= flask.request.form['x']
-        y= flask.request.form['y']
-        temp= flask.request.form['temp']
-        temp_min= flask.request.form['temp_min']
-        temp_max= flask.request.form['temp_max']
-        pressure= flask.request.form['pressure']
-        humidity= flask.request.form['humidity']
-        wind_speed= flask.request.form['wind_speed']
-        wind_deg= flask.request.form['wind_deg']
-        wind_gust= flask.request.form['wind_gust']
-        clouds_all= flask.request.form['clouds_all']
-        rain_1h= flask.request.form['rain_1h']
-        rain_3h= flask.request.form['rain_3h']
-        snow_1h= flask.request.form['snow_1h']
-        snow_3h= flask.request.form['snow_3h']
-        weather_main_Clear= flask.request.form['weather_main_Clear']
-        weather_main_Clouds= flask.request.form['weather_main_Clouds']
-        weather_main_Fog= flask.request.form['weather_main_Fog']
-        weather_main_Haze= flask.request.form['weather_main_Haze']
-        weather_main_Mist= flask.request.form['weather_main_Mist']
-        weather_main_Rain= flask.request.form['weather_main_Rain']
-        weather_main_Snow= flask.request.form['weather_main_Snow']
-        weather_main_Thunderstorm= flask.request.form['weather_main_Thunderstorm']
         
-        input_variables = pd.DataFrame(
-                        [[PULocationID,hour,dayofweek,dayofyear,month,quarter,x,y,temp,temp_min,temp_max,pressure,humidity,wind_speed,wind_deg,wind_gust,clouds_all,rain_1h,rain_3h,snow_1h,snow_3h,weather_main_Clear,weather_main_Clouds,weather_main_Fog,weather_main_Haze,weather_main_Mist,weather_main_Rain,weather_main_Snow,weather_main_Thunderstorm]],
-                        columns=['PULocationID','hour','dayofweek','dayofyear','month','quarter','x','y','temp','temp_min','temp_max','pressure','humidity','wind_speed','wind_deg','wind_gust','clouds_all','rain_1h','rain_3h','snow_1h','snow_3h','weather_main_Clear','weather_main_Clouds','weather_main_Fog','weather_main_Haze','weather_main_Mist','weather_main_Rain','weather_main_Snow','weather_main_Thunderstorm'],
-                        dtype=float,
-                        index=['input'])
+        input_variables = pd.DataFrame([[zone]],
+                                       columns=['zone'],
+                                       dtype=float,
+                                       index=['input'])
+
         data,scalar = ppd.preprocess_data(input_variables)
-        model = tf.keras.models.load_model("model/my_model.keras")
+        model = tf.keras.models.load_model("web app/model/my_model.keras")
         prediction_unscaled = model.predict(data)
         pred_list = scalar.inverse_transform(prediction_unscaled).tolist()
         prediction= pred_list[-1][0]
         prediction = round(prediction)
+        
         return flask.render_template('main.html',
-                                    original_input={'PULocationID':PULocationID,
-                                                     'Hour':hour,
-                                                     'Dayofweek':dayofweek,
-                                                     'Dayofyear':dayofyear,
-                                                     'Month':month,
-                                                     'Quarter':quarter,
-                                                     'X':x,
-                                                     'Y':y,
-                                                     'Temp':temp,
-                                                     'Temp_Min':temp_min,
-                                                     'Temp_Max':temp_max,
-                                                     'Pressure':pressure,
-                                                     'Humidity':humidity,
-                                                     'Wind_Speed':wind_speed,
-                                                     'Wind_Deg':wind_deg,
-                                                     'Wind_Gust':wind_gust,
-                                                     'Clouds_All':clouds_all,
-                                                     'Rain_1H':rain_1h,
-                                                     'Rain_3H':rain_3h,
-                                                     'Snow_1H':snow_1h,
-                                                     'Snow_3H':snow_3h,
-                                                     'Weather_Main_Clear':weather_main_Clear,
-                                                     'Weather_Main_Clouds':weather_main_Clouds,
-                                                     'Weather_Main_Fog':weather_main_Fog,
-                                                     'Weather_Main_Haze':weather_main_Haze,
-                                                     'Weather_Main_Mist':weather_main_Mist,
-                                                     'Weather_Main_Rain':weather_main_Rain,
-                                                     'Weather_Main_Snow':weather_main_Snow,
-                                                     'Weather_Main_Thunderstorm':weather_main_Thunderstorm},
-                                    result=prediction,)
-if __name__ == '__main__':
+                                     original_input={'Zone':zone},
+                                     result=prediction,
+                                     )
 
+if __name__ == '__main__':
     app.run()
--- web app/preprocess_data.py
+++ web app/preprocess_data.py
@@ -2,12 +2,14 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
-import datetime
-
+from datetime import datetime
+import pytz 
+import openmeteo_requests
+import requests_cache
+from retry_requests import retry
 
 # Preprocess the data
 def preprocess_data(newdata, target_column='PU_count'):
-    
     newdata = create_dataset(newdata)
     print(newdata.columns)
     newdata = newdata[['PU_count']] # Assuming 'PU_count' is the target column
@@ -19,40 +21,27 @@ def preprocess_data(newdata, target_column='PU_count'):
     print(newdata)
 
     return data, scalar
-    
-def generate_timestamp(hour, day_of_year):
-    """Generates a timestamp given the hour and day of the year in EST.
 
-    Args:
-        hour: The hour of the day (0-23).
-        day_of_year: The day of the year (1-366).
-
-    Returns:
-        A datetime object representing the timestamp in EST.
-        Returns None if input is invalid.
-    """
-    current_year = datetime.datetime.now().year
-
-    # Create the date string
-    date_str = f"{current_year}-{day_of_year}-{hour}"
-
-    # Attempt to parse the date, handling potential errors gracefully.
-    timestamp = pd.to_datetime(date_str, format='%Y-%j-%H') # Parse with UTC
-    return timestamp
 # Prepare data for LSTM
 def create_dataset(dataset, look_back=4):
     
     
-    
+
+    print(type(datetime))
     
     df = dataset.values.tolist()
     my_list = df[0]
     my_list.append(0)
+    timestamp = pd.to_datetime(datetime.now()).round(freq='H')
+    my_list= get_meteo_data(timestamp)
+    timestamp = timestamp -pd.DateOffset(years=1)
+    hour = timestamp.hour
+    day_of_year = timestamp.timetuple().tm_yday
     
     #do the lookback
     zone = int(df[0][0])
-    day_of_year = int(df[0][3])
-    hour = int(df[0][1])
+    #day_of_year = int(df[0][3])
+    #hour = int(df[0][1])
     print(zone, day_of_year, hour)
     
     test_path = "train_data.parquet"
@@ -60,7 +49,8 @@ def create_dataset(dataset, look_back=4):
     
     
     old_data.sort_index(inplace=True)
-    timestamp = generate_timestamp(hour, day_of_year)
+    #timestamp = generate_timestamp(hour, day_of_year)
+    
     matching_row = old_data.loc[(zone, timestamp),:].copy()
     matching_row.update(pd.Series(my_list))
     hour_list = []
@@ -83,8 +73,92 @@ def create_dataset(dataset, look_back=4):
     return filtered_df
         
 
+def get_meteo_data(timestamp): 
+        # Setup the Open-Meteo API client with cache and retry on error
+    cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+    retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+    openmeteo = openmeteo_requests.Client(session = retry_session)
+
+    # Make sure all required weather variables are listed here
+    # The order of variables in hourly or daily is important to assign them correctly below
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": 40.7143,
+        "longitude": -74.006,
+        "hourly": ["temperature_2m", "relative_humidity_2m", "rain", "snowfall", "weather_code", "surface_pressure", "cloud_cover", "wind_speed_10m", "wind_direction_10m", "wind_gusts_10m"],
+        "daily": ["temperature_2m_max", "temperature_2m_min"],
+        "temperature_unit": "fahrenheit",
+        "wind_speed_unit": "mph",
+        "precipitation_unit": "inch",
+        "timezone": "GMT",
+        "past_days": 1,
+        "forecast_days": 2
+    }
+    responses = openmeteo.weather_api(url, params=params)
+
+    # Process first location. Add a for-loop for multiple locations or weather models
+    response = responses[0]
+    print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+    print(f"Elevation {response.Elevation()} m asl")
+    print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+    print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+    # Process hourly data. The order of variables needs to be the same as requested.
+    hourly = response.Hourly()
+    hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+    hourly_relative_humidity_2m = hourly.Variables(1).ValuesAsNumpy()
+    hourly_rain = hourly.Variables(2).ValuesAsNumpy()
+    hourly_snowfall = hourly.Variables(3).ValuesAsNumpy()
+    hourly_weather_code = hourly.Variables(4).ValuesAsNumpy()
+    hourly_surface_pressure = hourly.Variables(5).ValuesAsNumpy()
+    hourly_cloud_cover = hourly.Variables(6).ValuesAsNumpy()
+    hourly_wind_speed_10m = hourly.Variables(7).ValuesAsNumpy()
+    hourly_wind_direction_10m = hourly.Variables(8).ValuesAsNumpy()
+    hourly_wind_gusts_10m = hourly.Variables(9).ValuesAsNumpy()
+
+    hourly_data = {"date": pd.date_range(
+        start = pd.to_datetime(hourly.Time(), unit = "s"),
+	    end = pd.to_datetime(hourly.TimeEnd(), unit = "s"),
+        freq = pd.Timedelta(seconds = hourly.Interval()),
+        inclusive = "left"
+    )}
+    
+    hourly_data["temperature_2m"] = hourly_temperature_2m
+    hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
+    hourly_data["rain"] = hourly_rain
+    hourly_data["snowfall"] = hourly_snowfall
+    hourly_data["weather_code"] = hourly_weather_code
+    hourly_data["surface_pressure"] = hourly_surface_pressure
+    hourly_data["cloud_cover"] = hourly_cloud_cover
+    hourly_data["wind_speed_10m"] = hourly_wind_speed_10m
+    hourly_data["wind_direction_10m"] = hourly_wind_direction_10m
+    hourly_data["wind_gusts_10m"] = hourly_wind_gusts_10m
+
+    hourly_dataframe = pd.DataFrame(data = hourly_data)
+    matching_rows = hourly_dataframe[hourly_dataframe['date']== timestamp].copy()
     
     
+    daily = response.Daily()
+    daily_temperature_2m_max = daily.Variables(0).ValuesAsNumpy()
+    daily_temperature_2m_min = daily.Variables(1).ValuesAsNumpy()
+
+    daily_data = {"date": pd.date_range(
+        start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
+        end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
+        freq = pd.Timedelta(seconds = daily.Interval()),
+        inclusive = "left"
+    )}
+    daily_data["temperature_2m_max"] = daily_temperature_2m_max
+    daily_data["temperature_2m_min"] = daily_temperature_2m_min
+
+    daily_dataframe = pd.DataFrame(data = daily_data)
+    matching_rows2 = daily_dataframe[daily_dataframe['date'].dt.date == timestamp.date()]
+    series1 = matching_rows.iloc[0]
+    series2 = matching_rows2.iloc[0]
+    series2.drop('date')
+    matching_rows = pd.concat([series1, series2])
+    print(matching_rows)
+
     
-    
-    
+timestamp = pd.to_datetime(datetime.now()).round(freq='H')
+get_meteo_data(timestamp)
